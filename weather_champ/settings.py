63925226bad2a98eaff3a673bd4bf021183a6a2e
hc <shreyashc018@gmail.com>
2020-05-03 19:50:43 +0530

    update

--- weather_champ/settings.py
+++ weather_champ/settings.py
@@ -114,10 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+import django_heroku
+django_heroku.settings(locals())
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
